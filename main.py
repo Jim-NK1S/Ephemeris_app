b849f80c3904b7b6acdc3a53ad64d5d21e6c435c
Jim_A <jimalix54@gmail.com>
2026-04-09 19:53:21 -0400

    Added azimuth info

--- main.py
+++ main.py
@@ -10,13 +10,15 @@ import csv
 import logging
 from pathlib import Path
 from datetime import date, timedelta
-
+import math
 import ephem
 from ephem import Observer
 
+from testing import tdy_start
+
 # ---------- Set up LOGGER
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.WARNING)
+logger.setLevel(logging.INFO)
 formatter = logging.Formatter("%(asctime)s:%(levelname)s:%(name)s:%(message)s")
 file_handler = logging.FileHandler("sunny_times.log")
 file_handler.setFormatter(formatter)
@@ -30,7 +32,7 @@ txt_file_path: Path = output_dir / "ephemeris_info.txt"
 csv_file_path: Path = output_dir / "daylight_info.csv"
 
 
-def main():
+def main(tdy):
     """Retrieves the time of sunrise and
     sunset and length of daylight for today,
     and computes the difference in amount of daylight
@@ -40,65 +42,64 @@ def main():
 
     # SET BASE INFORMATION FOR THE OBSERVER --->
     # Coordinates are for home @ 233 Liberty Ln, Harrisville RI:
+    start_tdy = ephem.Date(f'{tdy.year}/{tdy.month}/{tdy.day} 01:00:00')
     home: Observer = ephem.Observer()
     home.lat = "41.96247219"
     home.lon = "-71.677855830"
     home.elevation = 118  # in whole meters
-    given_date = date.today()
-    logger.info(f"Given date: {given_date}")
+    home.date = start_tdy
     sun = ephem.Sun()
     moon = ephem.Moon()
 
-    # Retrieve sun info for today:
-    sunrise = home.next_rising(sun, start=given_date)
+    # Retrieve sunrise and sunset times:
+    sunrise = home.next_rising(sun, start=start_tdy)
     local_sunrise = ephem.localtime(sunrise)
-    sunset = home.next_setting(sun, start=given_date)
+    sunset = home.next_setting(sun, start=start_tdy)
     local_sunset = ephem.localtime(sunset)
+    # Compute length of daylight:
     daylight: timedelta = local_sunset - local_sunrise
-    given_date_daylight: str = format_timedelta_hms(daylight)
-    logger.info(f"tdy_daylight {given_date_daylight}")
+    tdy_daylight: str = format_timedelta_hms(daylight)
     transit_time = home.next_transit(sun)  # solar noon
     tran_time = ephem.localtime(transit_time)  # solar noon local time
-    home.date = transit_time
-    max_alt_degrees = float(sun.alt) * 180 / ephem.pi  # sun's elevation at solar noon
-    logger.info(f"Max altitude: {max_alt_degrees:.2f} degrees")
-
-    # Times for civil twilight:
-    home.horizon = "-6"
-    am_twilight = ephem.localtime(home.next_rising(sun, start=given_date))
-    pm_twilight = ephem.localtime(home.next_setting(sun, start=given_date))
-
-    # CALCULATE daylight lost or gained after the last solstice ==>
-    # Reset horizon for actual sun rise/set:
-    home.horizon = "0"
-    last_solstice = find_previous_solstice(given_date)
-    logger.info(f"Last solstice: {last_solstice}")
-
-    # Retrieves length of daylight on last solstice:
+    # Compute solar noon and max elevation:
+    max_alt_degrees = float(sun.alt) * 180 / ephem.pi
+    max_alt_deg_2 = round(math.degrees(float(sun.alt)), 1)
+    # Get date of last solstice (horizon still 0º ):
+    last_solstice = find_previous_solstice(tdy)
     sol_daylight: timedelta = get_daylight_diff(last_solstice[0], home)
 
-    # Calculates difference between today and solstice:
+    # Calculates difference in daylight between today and solstice:
     if last_solstice[1] == "summer":
         diff = format_timedelta_hms(sol_daylight - daylight)
     else:
         diff = format_timedelta_hms(daylight - sol_daylight)
 
-    # Retrieve info about Moon:
-    moon_rise = home.next_rising(moon, start=given_date)
-    full_moon = ephem.next_full_moon(given_date)
-    new_moon = ephem.next_new_moon(given_date)
+    # Times for civil twilight:
+    home.horizon = "-6"
+    am_twilight = ephem.localtime(home.next_rising(sun, start=start_tdy))
+    pm_twilight = ephem.localtime(home.next_setting(sun, start=start_tdy))
 
-    logger.info(f"Last solstice 1: {last_solstice[1]}")
-    logger.info(f"Last solstice 0: {last_solstice[0]}")
-    logger.info(
-        f"today_length: {sol_daylight}; solstice_length: {daylight}; diff: {diff}"
-    )
+    home.date = sunrise
+    home.horizon = "0"
+    sun.compute(home)
+    azimuth_rise = (round(math.degrees(float(sun.az)), 1))
+    home.date = sunset
+    home.horizon = "0"
+    sun.compute(home)
+    azimuth_set = (round(math.degrees(float(sun.az)), 1))
+
+    # Retrieve info about Moon:
+    moon_rise = home.next_rising(moon, start=start_tdy)
+    full_moon = ephem.next_full_moon(start_tdy)
+    new_moon = ephem.next_new_moon(start_tdy)
 
     # Set up variables for printing:
-    date_today = given_date.strftime("%a, %b %d, %Y")
+    date_today = tdy.strftime("%a, %b %d, %Y")
     first_light = "Civil Twilight"
     rise = "Sunrise"
+    az_rise = "Azimuth @ sunrise"
     set_ = "Sunset"
+    az_set = "Azimuth @ sunset"
     last_light = "Civil Twilight"
     length = "Length of Daylight"
     solar_noon = "Solar Noon"
@@ -115,13 +116,15 @@ Information for {date_today}
 
 {first_light:.<24} {am_twilight.strftime("%H:%M:%S")}
 {rise:.<24} {local_sunrise.strftime("%H:%M:%S")}
+{az_rise:.<24} {azimuth_rise}
 {set_:.<24} {local_sunset.strftime("%H:%M:%S")}
+{az_set:.<24} {azimuth_set}
 {last_light:.<24} {pm_twilight.strftime("%H:%M:%S")}
 
 {solar_noon:.<24} {tran_time.strftime("%H:%M:%S")}
-{max_elev:.<24} {max_alt_degrees:.2f}º
+{max_elev:.<24} {max_alt_deg_2:.1f}º
 
-{length:.<24} {given_date_daylight}
+{length:.<24} {tdy_daylight}
 
 {last:.<24} {last_solstice[0].strftime("%a, %b %d @ %H:%M")}
 {lost:.<24} {diff}
@@ -136,11 +139,13 @@ Information for {date_today}
     csv_data = {
         "Date": date.today(),
         "Sunrise": local_sunrise.strftime("%H:%M:%S"),
+        "Azimuth @ Sunrise": azimuth_rise,
         "Sunset": local_sunset.strftime("%H:%M:%S"),
+        "Azimuth @ Sunset": azimuth_set,
         "Diff from Solstice": diff,
         "Solar Noon": tran_time.strftime("%H:%M:%S"),
         "Sun Elevation": round(max_alt_degrees, 2),
-        "Length of Daylight": given_date_daylight,
+        "Length of Daylight": tdy_daylight,
     }
     # ****************************************
 
@@ -152,15 +157,15 @@ Information for {date_today}
     # Print to text file:
     with open(txt_file_path, "w", encoding="utf-8") as file:
         file.write(results + "\n")
-
-    # Print to csv file:
-    file_exists = csv_file_path.exists()
-    with open(csv_file_path, mode="a", newline="", encoding="utf-8") as f:
-        writer = csv.DictWriter(f, fieldnames=csv_data.keys())
-        if not file_exists:
-            writer.writeheader()
-
-        writer.writerow(csv_data)
+    #
+    # # Print to csv file:
+    # file_exists = csv_file_path.exists()
+    # with open(csv_file_path, mode="a", newline="", encoding="utf-8") as f:
+    #     writer = csv.DictWriter(f, fieldnames=csv_data.keys())
+    #     if not file_exists:
+    #         writer.writeheader()
+    #
+    #     writer.writerow(csv_data)
 
 
 # ****************************************
@@ -173,12 +178,12 @@ def find_previous_solstice(given_date: date) -> tuple:
     prev_winter = None
 
     try:
-        prev_summer = ephem.previous_summer_solstice(given_date)
+        prev_summer = ephem.previous_summer_solstice(tdy_start)
     except Exception as e:
         logger.warning("Could not determine previous summer solstice: %s", e)
 
     try:
-        prev_winter = ephem.previous_winter_solstice(given_date)
+        prev_winter = ephem.previous_winter_solstice(tdy_start)
     except Exception as e:
         logger.warning("Could not determine previous winter solstice: %s", e)
 
@@ -213,10 +218,11 @@ def format_timedelta_hms(td: timedelta) -> str:
 
 
 if __name__ == "__main__":
-    if csv_file_path.exists():
-        if date.fromtimestamp(csv_file_path.stat().st_mtime) < date.today():
-            main()
-        else:
-            print("\n\nYOU'VE ALREADY RUN THE SCRIPT TODAY !!! \n")
-    else:
-        main()
+    tdy = date.today()
+    # if csv_file_path.exists():
+    #     if date.fromtimestamp(csv_file_path.stat().st_mtime) < tdy:
+    #         main()
+    #     else:
+    #         print("\n\nYOU'VE ALREADY RUN THE SCRIPT TODAY !!! \n")
+    # else:
+    main(tdy)
